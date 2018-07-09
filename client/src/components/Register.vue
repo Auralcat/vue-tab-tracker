87ae394710aa81f3bb9f9b57f5b9629d23234ac5
Auralcat <ashleyzt@hi2.in>
2018-07-09 12:51:57 -0300

    Bind password and email fields

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>Register</h1>
+    <input name="email" type="email" value="" v-model="email" placeholder="email"/>
+    <br>
+    <input name="password" type="password" value="" v-model="password" placeholder="password"/>
+    <br>
+    <button>Register</button>
   </div>
 </template>
 
@@ -8,7 +13,8 @@
 export default {
   data () {
     return {
-
+      email: '',
+      password: ''
     }
   }
 }
