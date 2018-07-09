a35bb7732948d16d5752811fe2bea2d78c7c4442
Auralcat <ashleyzt@hi2.in>
2018-07-09 11:54:58 -0300

    Link deps to app and explain them

--- server/src/app.js
+++ server/src/app.js
@@ -2,3 +2,17 @@ const express = require('express')
 const bodyParser = require('bodyParser')
 const morgan = require('morgan')
 const cors = require('cors')
+
+const app = express()
+
+// Use Morgan as log generator
+app.use(morgan('combine'))
+
+// Allow our Express app to easily parse any JSON requests sent
+app.use(bodyParser.json())
+
+// Enable CORS
+app.use(cors())
+
+// Start listening
+app.listen(process.env.PORT | 8081)
