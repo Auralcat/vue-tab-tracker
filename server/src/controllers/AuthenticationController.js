dc45441e111230f6eb2fd7a2737c3ee1cedbf8e3
Auralcat <ashleyzt@hi2.in>
2018-07-10 10:02:41 -0300

    Some refactoring, update sqlite3

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,16 +1,16 @@
-const {User} = require('../models')
+const {User} = require('../models');
 
 module.exports = {
-    async register (req, res) {
+    register: async function(req, res) {
         try {
-            const user = await User.create(req.body)
-            res.send(user.toJSON())
+            const user = await User.create(req.body);
+            res.send(user.toJSON());
         } catch (err) {
             res.status(400).send({
                 error: 'This email account is already in use.',
                 debug: err,
                 incomingRequest: req.body
-            })
+            });
         }
     }
 }
