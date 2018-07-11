082d69c02def016bfafb176574eb8f7ce31bdcbd
Auralcat <ashleyzt@hi2.in>
2018-07-11 17:24:29 -0300

    Wrap fields in form, add autocomplete field to password input

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,22 +1,21 @@
 <template>
   <v-layout column>
-    <v-flex xs6 offset-xs3>
-      <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
-        </v-toolbar>
-        <div class="pl-4 pr-4 pt-2 pb-2">
-          <v-text-field class="my-input-box" label="Email" v-model="email"></v-text-field>
-          <v-text-field class="my-input-box"
-            label="Password"
-            v-model="password"
-            :type="show1 ? 'text' : 'password'"/>
-            <div class="error" v-html="error"></div>
-            <br>
-            <v-btn class="cyan" @click="register" dark>Register</v-btn>
-        </div>
-      </div>
-    </v-flex>
+      <v-flex xs6 offset-xs3>
+          <div class="white elevation-2">
+              <v-toolbar flat dense class="cyan" dark>
+                  <v-toolbar-title>Register</v-toolbar-title>
+              </v-toolbar>
+              <div class="pl-4 pr-4 pt-2 pb-2">
+                  <form class="tab-tracker-form">
+                      <v-text-field class="my-input-box" label="Email" v-model="email" />
+                      <v-text-field class="my-input-box" label="Password" v-model="password" :type="show1 ? 'text' : 'password'" autocomplete="new-password" />
+                  </form>
+                  <div class="error" v-html="error"></div>
+                  <br>
+                  <v-btn class="cyan" @click="register" dark>Register</v-btn>
+              </div>
+          </div>
+      </v-flex>
   </v-layout>
 </template>
 
