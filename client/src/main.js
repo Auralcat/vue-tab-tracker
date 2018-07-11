984194812891751c494f5760b807ab0ff93e103d
Auralcat <ashleyzt@hi2.in>
2018-07-11 17:33:41 -0300

    Create store.js

--- client/src/main.js
+++ client/src/main.js
@@ -4,12 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
+import { sync } from 'vuex-router-sync'
 import 'vuetify/dist/vuetify.min.css'
+import store from '@/store/store'
 
 Vue.config.productionTip = false
 
 // Enable Vuetify
 Vue.use(Vuetify)
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
