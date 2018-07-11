7d9f2eb7ce1164d5c745200a4d16147913575c14
Auralcat <ashleyzt@hi2.in>
2018-07-11 17:47:29 -0300

    Add store to Vue, add isUserLoggedIn var

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -7,11 +7,18 @@ export default new Vuex.Store({
   strict: true,
   state: {
     token: null,
-    user: null
+    user: null,
+    isUserLoggedIn: false
   },
   mutation: {
     setToken (state, token) {
       state.token = token
+      state.isUserLoggedIn = Boolean(token)
+      // if (token) {
+      //   state.isUserLoggedIn = true
+      // } else {
+      //   state.isUserLoggedIn = false
+      // }
     },
     setUser (state, token) {
       state.token = token
