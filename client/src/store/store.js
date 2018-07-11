836968cd53955edb9d4cda28871b27eff97c2ca2
Auralcat <ashleyzt@hi2.in>
2018-07-11 17:43:28 -0300

    Add setUser method to store.js

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -7,15 +7,23 @@ export default new Vuex.Store({
   strict: true,
   state: {
     token: null,
+    user: null
   },
   mutation: {
     setToken (state, token) {
+      state.token = token
+    },
+    setUser (state, token) {
       state.token = token
     }
   },
   actions: {
     setToken ({commit}, token) {
       commit('setToken', token)
+    },
+    setUser ({commit}, token) {
+      commit('setUser', token)
     }
+
   }
 })
