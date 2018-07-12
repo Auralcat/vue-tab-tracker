26067f993ddecda30b9439e852190443071f3d91
Auralcat <ashleyzt@hi2.in>
2018-07-12 12:15:54 -0300

    Plug SongsService to Songs.vue

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -22,6 +22,7 @@
 </template>
 
 <script>
+import SongsService from "@/services/SongsService"
 import Panel from '@/components/Panel'
 export default {
   components: {
@@ -32,8 +33,9 @@ export default {
         songs: null
       }
     },
-    mounted () {
+    async mounted () {
       /* Do a request to the backend for all the songs */
+      this.songs = await SongsService.index()
     }
 }
 </script>
