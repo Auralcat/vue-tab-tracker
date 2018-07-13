665c7e57d3a0ef7f107fdf9112342545e3118e1e
Auralcat <ashleyzt@hi2.in>
2018-07-13 10:22:00 -0300

    Create post and index methods for SongsController

--- server/src/controllers/SongsController.js
+++ server/src/controllers/SongsController.js
@@ -3,7 +3,17 @@ const {Song} = require('../models');
 module.exports = {
     async index (req, res) {
         try {
+            const song = Song.findAll({
+                limit: 10
+            })
+        } catch (err) {
 
+        }
+    },
+    async post (req, res) {
+        try {
+            const song = Song.create(req.body)
+            res.send(song)
         } catch (err) {
             res.status(500).send({
                 error: "An error has occurred while trying to get songs"
