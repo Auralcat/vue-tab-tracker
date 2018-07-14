937e020bdc8b82f005b9e8285a2765fd279c0315
Auralcat <ashleyzt@hi2.in>
2018-07-13 21:15:09 -0300

    Work in CreateSong.vue

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -3,9 +3,21 @@
 </template>
 
 <script>
-  export default {
-
-  }
+import Panel from '@/components/Panel'
+export default {
+    data () {
+      return {
+          title: '',
+          artist: '',
+          genre: '',
+          album: '',
+          albumImageUrl: '',
+          youtubeId: '',
+          lyrics: '',
+          tab: '',
+      }
+    }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
