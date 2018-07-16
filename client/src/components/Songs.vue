ba8ea95b8080882f3630a1f3b9237990d3009dcf
Auralcat <ashleyzt@hi2.in>
2018-07-15 21:04:21 -0300

    Style Songs.vue

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -15,10 +15,19 @@
         <v-btn @click="navigateTo({name: 'songs-create'})" absolute class="cyan accent-2" fab light medium middle right slot="action">
           <v-icon>add</v-icon>
         </v-btn>
-        <div v-for="song in songs" :key="song.id">
-          {{song.title}} -
-          {{song.artist}} -
-          {{song.album}}
+        <div class="song" v-for="song in songs" :key="song.id">
+
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">{{song.title}}</div>
+              <div class="song-artist">{{song.artist}}</div>
+              <div class="song-genre">{{song.genre}}</div>
+            </v-flex>
+            <v-flex xs6>
+              <img class="album-image" alt="Album Image" :src="song.albumImageUrl"/>
+            </v-flex>
+          </v-layout>
+
         </div>
       </panel>
     </v-flex>
@@ -51,5 +60,26 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .song {
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+  }
 
+  .song-title {
+    font-size: 30px;
+  }
+
+  .song-artist {
+    font-size: 24px;
+  }
+
+  .song-genre {
+    font-size: 18px;
+  }
+
+  .album-image {
+    width: 70%;
+    margin: 0 auto;
+  }
 </style>
