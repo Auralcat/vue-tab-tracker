daa5520ff2de3ff2c89084d70770b4a36c0c28ca
Auralcat <ashleyzt@hi2.in>
2018-07-17 11:58:48 -0300

    Forgot to add file

---
+++ client/src/components/ViewSong/YouTube.vue
@@ -0,0 +1,20 @@
+<template>
+  <panel title="YouTube Video">{{youtubeId}}</panel>
+</template>
+
+<script>
+  import Panel from '@/components/Panel'
+  export default {
+    props: [
+      'youtubeId'
+    ],
+    components: {
+      Panel
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
