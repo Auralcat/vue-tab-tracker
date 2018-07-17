3e1cfccfd43c5c6c9e59f840aa1b3d8ba63d5a3a
Auralcat <ashleyzt@hi2.in>
2018-07-17 10:12:43 -0300

    Add show song method

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -3,8 +3,13 @@
 </template>
 
 <script>
+  import SongsService from '@/services/SongsService'
   export default {
-
+    async mounted () {
+      const songId = this.$store.state.route.params.songId
+      this.song = await SongsService.show(songId)
+      console.log(song)
+    }
   }
 </script>
 
--- client/src/services/SongsService.js
+++ client/src/services/SongsService.js
@@ -5,6 +5,9 @@ export default {
   index () {
     return Api().get('songs')
   },
+  show (songId) {
+    return Api().get(`songs/${songId}`)
+  },
   post (song) {
     return Api().post('songs', song)
   }
