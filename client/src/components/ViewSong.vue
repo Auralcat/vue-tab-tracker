cdec492071ff8a59bc61c6d49035ab935fa84fa5
Auralcat <ashleyzt@hi2.in>
2018-07-17 10:59:55 -0300

    Format data display inside ViewSong component

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,11 +1,17 @@
 <template>
-  <panel title="Song Metadata">
-    <div>
-      {{song.title}}
-      {{song.artist}}
-      {{song.genre}}
-    </div>
-  </panel>
+  <v-layout>
+    <v-flex xs6>
+      <panel title="Song Metadata">
+        <div class="song-title">{{ song.title  }}</div>
+        <div class="song-artist">{{ song.artist }}</div>
+        <div class="song-genre">{{ song.genre }}</div>
+        <v-btn class="cyan" @click="navigateTo({name: 'song', params: {songId: song.id}})">View</v-btn>
+      </panel>
+    </v-flex>
+    <v-flex>
+      <img class="album-image" alt="album image" :src="song.albumImageUrl"/>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
