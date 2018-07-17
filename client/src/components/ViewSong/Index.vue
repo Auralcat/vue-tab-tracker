5b2ac1ac111110421d4f3eb6524b88c02f37ec6b
Auralcat <ashleyzt@hi2.in>
2018-07-17 11:54:02 -0300

    Put song metadata into component

--- client/src/components/ViewSong/Index.vue
+++ client/src/components/ViewSong/Index.vue
@@ -2,18 +2,7 @@
   <div>
     <v-layout>
       <v-flex xs6>
-        <panel title="Song Metadata">
-          <v-layout>
-            <v-flex xs6>
-              <div class="song-title">{{ song.title  }}</div>
-              <div class="song-artist">{{ song.artist }}</div>
-              <div class="song-genre">{{ song.genre }}</div>
-            </v-flex>
-            <v-flex xs6>
-              <img class="album-image" alt="album image" :src="song.albumImageUrl"/>
-            </v-flex>
-          </v-layout>
-        </panel>
+        <song-metadata/>
       </v-flex>
 
       <v-layout>
@@ -34,6 +23,7 @@
 </template>
 
 <script>
+  import SongMetadata from './SongMetadata'
   import SongsService from '@/services/SongsService'
   import Panel from '@/components/Panel'
   export default {
@@ -48,36 +38,14 @@
       console.log(this.song)
     },
     components: {
-      Panel
+      Panel,
+      SongMetadata
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .song {
-    padding: 20px;
-    height: 330px;
-    overflow: hidden;
-  }
-
-  .song-title {
-    font-size: 30px;
-  }
-
-  .song-artist {
-    font-size: 24px;
-  }
-
-  .song-genre {
-    font-size: 18px;
-  }
-
-  .album-image {
-    width: 70%;
-    margin: 0 auto;
-  }
-
   .tab-area {
     width: 100%;
     font-family: monospace;
