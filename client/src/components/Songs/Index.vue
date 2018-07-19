422468415414c58940b4440d5c14ec5b9a1435c3
Auralcat <ashleyzt@hi2.in>
2018-07-19 16:06:09 -0300

    Split Songs.vue into smaller components

---
+++ client/src/components/Songs/Index.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <songs-search-panel/>
+      <songs-panel/>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import SongsService from '@/services/SongsService'
+import SongsPanel from './SongsPanel'
+import SongsSearchPanel from './SongsSearchPanel'
+import Panel from '@/components/Panel'
+export default {
+  components: {
+    Panel,
+    SongsPanel,
+    SongsSearchPanel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  },
+  async mounted () {
+    /* Do a request to the backend for all the songs */
+    this.songs = (await SongsService.index()).data
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .song {
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+  }
+
+  .song-title {
+    font-size: 30px;
+  }
+
+  .song-artist {
+    font-size: 24px;
+  }
+
+  .song-genre {
+    font-size: 18px;
+  }
+
+  .album-image {
+    width: 70%;
+    margin: 0 auto;
+  }
+</style>
