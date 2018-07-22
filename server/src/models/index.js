6eb8906f339d12560725b3c61b30ba7b853088d6
Auralcat <ashleyzt@hi2.in>
2018-07-22 20:10:01 -0300

    Start model association

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -22,6 +22,13 @@ fs
     db[model.name] = model
   })
 
+// Model associations
+Object.key(db).forEach(function (modelName) {
+  if ('associate' in db[modelName]) {
+    db[modelName].associate(db)
+  }
+})
+
 db.sequelize = sequelize
 db.Sequelize = Sequelize
 
