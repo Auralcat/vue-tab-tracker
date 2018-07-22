df7250bb2b8b0c19f82e5b41553c35ede997a125
Auralcat <ashleyzt@hi2.in>
2018-07-22 20:04:19 -0300

    Seed database

--- server/seed/index.js
+++ server/seed/index.js
@@ -2,14 +2,14 @@ const {
   sequelize,
   Song,
   User
-} = require('/models')
+} = require('../src/models')
 
 const Promise = require('bluebird')
 const songs = require('./songs.json')
 const users = require('./users.json')
 
 sequelize.sync({force: true})
-  .then(async function {
+  .then(async function () {
     await Promise.all(
       users.map(user => {
         User.create(user)
