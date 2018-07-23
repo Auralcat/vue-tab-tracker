122aed5bf744dece7baf2aec00310e41f6ebdd7b
Auralcat <ashleyzt@hi2.in>
2018-07-22 21:34:40 -0300

    Add userId to coords

--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -3,10 +3,11 @@ const {Bookmark} = require('../models')
 module.exports = {
   async index (req, res) {
     try {
-      const {songId} = req.query
+      const {songId, userId} = req.query
       const bookmark = await Bookmark.findOne({
         where: {
-          SongId: songId
+          SongId: songId,
+          UserId: userId
         }
       })
       res.send(bookmark)
