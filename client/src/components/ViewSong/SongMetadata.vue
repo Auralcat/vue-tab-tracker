a700dd4aedd3dcb7eeb033c0b7be6c46dae796a2
Auralcat <ashleyzt@hi2.in>
2018-07-22 22:05:18 -0300

    Plug bookmark buttons

--- client/src/components/ViewSong/SongMetadata.vue
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -7,8 +7,8 @@
         <div class="song-genre">{{ song.genre }}</div>
 
         <v-btn class="cyan" dark :to="{name: 'song-edit', params () { return { songId: song.id }}}">Edit</v-btn>
-        <v-btn v-if="$store.state.isUserLoggedIn" class="cyan" dark @click="bookmark">Bookmark</v-btn>
-        <v-btn v-if="$store.state.isUserLoggedIn" class="cyan" dark @click="unbookmark">Unbookmark</v-btn>
+        <v-btn v-if="$store.state.isUserLoggedIn && !isBookmarked" class="cyan" dark @click="bookmark">Bookmark</v-btn>
+        <v-btn v-if="$store.state.isUserLoggedIn && isBookmarked" class="cyan" dark @click="unbookmark">Unbookmark</v-btn>
       </v-flex>
       <v-flex xs6>
         <img class="album-image" alt="album image" :src="song.albumImageUrl"/>
@@ -22,6 +22,11 @@ import {mapState} from 'vuex'
 import BookmarksService from '@/services/BookmarksService'
 
 export default {
+  data () {
+    return {
+      isBookmarked: false
+    }
+  },
   props: [
     'song'
   ],
@@ -32,9 +37,14 @@ export default {
       'isUserLoggedIn'
     ])
   },
-  mounted() {
-
-  }
+  async mounted() {
+    const bookmark = (await BookmarksService.index({
+      songId: 1,
+      userId: 1
+    })).data
+    this.isBookmarked = !!bookmark
+    console.log('bookmark', this.isBookmarked)
+  },
   methods: {
     bookmark () {
       console.log("Bookmarked!")
