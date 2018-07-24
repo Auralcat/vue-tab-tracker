549c8d06973091ec588bb65c2ad72dddf380870c
Auralcat <ashleyzt@hi2.in>
2018-07-24 16:52:16 -0300

    Create bookmark controller methods

--- client/src/services/BookmarksService.js
+++ client/src/services/BookmarksService.js
@@ -1,14 +1,14 @@
 import Api from '@/services/Api'
 
 export default {
-  index (params) {
+  index (bookmark) {
     return Api().get('bookmarks', {
-      params: params
+      params: bookmark
     })
   },
-  post (bookmark) {
+  post (params) {
     return Api().post('bookmarks', {
-      params: bookmark
+      params: params
     })
   },
   delete (bookmarkId) {
--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -17,4 +17,27 @@ module.exports = {
       })
     }
   },
+  async post (req, res) {
+    try {
+      const bookmark = req.body
+      await Bookmark.create(bookmark)
+      res.send(bookmark)
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to create the bookmark'
+      })
+    }
+  },
+  async delete (req, res) {
+    try {
+      const {bookmarkId} = req.params
+      const bookmark = await Bookmark.findById(bookmarkId)
+      await bookmark.destroy()
+      res.send(bookmark)
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to delete the bookmark'
+      })
+    }
+  }
 }
