1a8a65d5edd1a5ddbbdaccfa2fc7b66c22916f1f
Auralcat <ashleyzt@hi2.in>
2018-07-26 12:55:57 -0300

    Done with server, final touches in the UI

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,8 +1,12 @@
 // This is to make the API management easier
 import axios from 'axios'
+import store from '@/store/store'
 
 export default() => {
   return axios.create({
-    baseURL: 'http://localhost:8081/'
+    baseURL: 'http://localhost:8081/',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
   })
 }
