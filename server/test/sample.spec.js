b0046bbd6a0134706338330488042cb080e53e1b
Auralcat <ashleyzt@hi2.in>
2018-08-14 17:25:55 -0300

    Lay more tests

--- server/test/sample.spec.js
+++ server/test/sample.spec.js
@@ -12,6 +12,50 @@ describe("Songs endpoints", function() {
       .expect('Content-Type', /json/)
       .expect(200, done)
   });
+})
 
+describe("Bookmark endpoints", function() {
+  it("GET returns a JSON object", function(done) {
+    request(app)
+      .get('/songs')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+  });
+})
 
+describe("History endpoints", function() {
+  it("GET returns a JSON object", function(done) {
+    request(app)
+      .get('/songs')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+  });
+})
+
+describe("Register endpoint", function() {
+  it("POST (registration) returns a JSON object", function(done) {
+    request(app)
+      .post('/register')
+      .send({email: 'some-email@valid-email.com', password: 'supersecurepassword'})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+        done();
+      })
+  })
+})
+
+describe("Login endpoint", function() {
+  it("GET returns a JSON object", function(done) {
+    request(app)
+      .get('/songs')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+  })
 });
