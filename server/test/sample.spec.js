44b22a7c47b728941df6ce43f1e3601f13b14288
Auralcat <ashleyzt@hi2.in>
2018-08-14 17:17:30 -0300

    Add supertest, write test for songs endpoint

--- server/test/sample.spec.js
+++ server/test/sample.spec.js
@@ -1,3 +1,17 @@
-describe("Sample test", function() {
-  console.log("Test is running");
+// What needs to be tested:
+// - Endpoints are working properly (POST, GET, PUT)
+const request = require('supertest');
+const express = require('express');
+const app = express()
+
+describe("Songs endpoints", function() {
+  it("GET returns a JSON object", function(done) {
+    request(app)
+      .get('/songs')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+  });
+
+
 });
